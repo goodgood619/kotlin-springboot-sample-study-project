9ae3d51639929120d5e7b6e94b567a8b4e6cbd3d
yoonseok <yoonseok.lee@travel-wallet.com>
2023-01-22 20:35:59 +0900

    n+1 문제 해결 하기 위한 fetch join 문 작성

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,9 +1,12 @@
 package com.group.libraryapp.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
+import org.springframework.data.jpa.repository.Query
 
 interface UserRepository : JpaRepository<User, Long> {
 
     fun findByName(name: String) : User?
+
+    @Query("select distinct u FROM User u LEFT JOIN FETCH u.userLoanHistories")
+    fun findAllWithHistories(): List<User>
 }
